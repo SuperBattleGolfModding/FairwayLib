90cd69d6476032cd3d3645510694a581123d455a
Sulayre <jose.alonso.pares@gmail.com>
2026-03-03 01:27:51 -0300

    Plugins Source + Assets Folder
    missing the assembly references for the game's code, not sure if there's a fancy way for that

--- src/FairwayLib.Item/Plugin.cs
+++ src/FairwayLib.Item/Plugin.cs
@@ -1,9 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using FairwayLib.Core;
 
 namespace FairwayLib.Item
 {
     [BepInAutoPlugin]
+    [BepInDependency(CorePlugin.Id)]
     public partial class ItemPlugin : BaseUnityPlugin
     {
         internal static ManualLogSource Log { get; private set; } = null!;
